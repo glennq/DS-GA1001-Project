f1d3ef0b9a8704109df0adac8b7fa8e973837843
Glenn Qian <qianjy1990@gmail.com>
2014-11-29 04:38:16 -0500

    added deployment

---
+++ Deployment_Qian/functions.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+def addPriceDiff(df):
+    """
+    This function takes as input a pandas DataFrame, and calculates price differences between the eight
+    reference prices and the purchasing price in place.
+    It does not return anything
+    """
+    # Create list for columns names that contains the eight reference prices
+    names = ["MMRAcquisitionAuctionAveragePrice", "MMRAcquisitionAuctionCleanPrice", \
+             "MMRAcquisitionRetailAveragePrice", "MMRAcquisitonRetailCleanPrice", \
+             "MMRCurrentAuctionAveragePrice", "MMRCurrentAuctionCleanPrice", \
+             "MMRCurrentRetailAveragePrice", "MMRCurrentRetailCleanPrice", "VehBCost"]
+    # Create list for abbreviations of the eight column names to be used in creating new features
+    abbrs = ['AAAP', 'AACP', 'ARAP', 'ARCP', 'CAAP', 'CACP', 'CRAP', 'CRCP', 'VehBCost']
+    # Calculate difference between each other and store in df
+    for i in range(len(names)):
+        for j in range(i+1, len(names)):
+            if i != j:
+                df[abbrs[i] + '_' + abbrs[j] + '_diff'] = df[names[i]] - df[names[j]]
+                
+def transformVar(df):
+    """
+    This function takes as input a pandas DataFrame and transforms all categorical variables to dummy variables
+    It returns a new DataFrame with all categorical variables replaced by dummy variables
+    """
+    # Transform to dummy variables
+    for i in df.columns:
+        if df[i].dtype == 'object':
+            temp = df[i].value_counts()
+            df = pd.concat([df, pd.get_dummies(df[i], prefix=i)], axis=1)
+            df = df.drop(i, axis=1)
+    return df
+
+def standardizeDF(df, mean, std):
+    """
+    This function takes as input two pandas DataFrames and standardizes all numerical (non-binary non-date) columns in place
+    Date and year are left unstandardized for further analysis
+    There's no return value
+    """
+    # Identify columns to standardize
+    toStd = df.columns[range(0, 11) + range(12, 49)]
+    # Standardize both train_X and test_X with the same parameters
+    df[toStd] = (df[toStd] - mean) / std
---
+++ Deployment_Qian/webapp.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, render_template, flash
+from sklearn.externals import joblib
+from sklearn import linear_model
+from olform import *
+from functions import *
+import cPickle
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def inputData():
+    form = ReportForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # read helper data which are in two dictionaries
+        pk_file = open('helper.pkl', 'rb')
+        helper = cPickle.load(pk_file)
+        pk_file.close()
+        pk_file2 = open('helper2.pkl', 'rb')
+        helper2 = cPickle.load(pk_file2)
+        pk_file2.close()
+        # read classifier
+        clf = joblib.load('Lr.pkl') 
+        # create dataframe from form data
+        df = pd.DataFrame(form.data, index=[0])
+        # add column TopThreeAmericanName
+        TopThree = "OTHER"
+        if df.Make[0] in helper["GM"]:
+            TopThree = "GM"
+        elif df.Make[0] in helper["FORD"]:
+            TopThree = "FORD"
+        elif df.Make[0] in helper["CHRYSLER"]:
+            TopThree = "CHRYSLER"
+        df["TopThreeAmericanName"] = TopThree
+        # Change Make value to OTHER if in minority
+        if df.Make[0] in helper['Make_OTHER']:
+            df.Make[0] = 'OTHER'
+        # Add price differences
+        addPriceDiff(df)
+        # transform categorical variables
+        df = transformVar(df)
+        # reorder columns
+        df = df.reindex_axis(helper2['columns'], axis=1)
+        df = df.fillna(0, axis=0)
+        # standardize data
+        standardizeDF(df, helper2['mean'], helper2['std'])
+        # predict using classifier
+        prob = clf.predict_proba(np.float64(df))
+        # output result
+        flash(str(prob[:, 1]))
+    return render_template('form.html', form=form)
+
+if __name__ == '__main__':
+    app.secret_key = 'why would I tell you my secret k'
+    app.run()
